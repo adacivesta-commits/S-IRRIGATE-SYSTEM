82cfdff3511cdb8a9c2a634305dcf36c8eca125b
adacivesta-commits <adacivesta@gmail.com>
2026-06-08 11:41:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Ganti IP ini dengan IP ESP32 kamu nanti
-const ESP32_IP = "http://10.191.213.100";
+const ESP32_IP = "http://10.127.174.190";
 
 // Setup Chart
 const ctx = document.getElementById('myChart').getContext('2d');
